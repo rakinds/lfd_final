fc45c86403fdc7dd774ad626ab3e5fd3d7eb9057
rakinds <kindsra@gmail.com>
2024-10-26 10:06:40 +0200

    Added MHS code

--- classic_models.py
+++ classic_models.py
@@ -4,8 +4,8 @@
 LfD Final Assignment - Naive Bayes
 
 Available command-line options:
--t Define the train file to use. Uses data/train.tsv defuault.
--d Define the dev / test file to use. Uses data/dev.tsv defuault
+-t Define the train file to use. Uses data/train.tsv default.
+-d Define the dev / test file to use. Uses data/dev.tsv default
 -p Displays a plotted version of the confusion matrix for the report. May not always work
                              from the commandline if display packages are not installed.
 -a Augmented: run the NB model with augmented features
@@ -26,22 +26,52 @@ from sklearn.metrics import classification_report
 from sklearn.metrics import confusion_matrix, ConfusionMatrixDisplay
 import matplotlib.pyplot as plt
 from collections import Counter
+import datasets
+import string
+import re
 
 
 def create_arg_parser():
     """Creates argumentparser and defines command-line options that can be called upon."""
     parser = argparse.ArgumentParser()
     parser.add_argument("-t", "--train_file", default='data/train.tsv', type=str,
-                        help="Train file (default: data/train.txt)")
+                        help="Train file (default: data/train.tsv)")
     parser.add_argument("-d", "--dev_file", default='data/dev.tsv', type=str,
-                        help="Dev/test file (default: data/dev.txt)")
+                        help="Dev/test file (default: data/dev.tsv)")
     parser.add_argument("-p", "--plot_show", action="store_true",
                         help="Displays a plotted version of the confusion matrix for the report. May not always work"
                              "from the commandline if display packages are not installed.")
     parser.add_argument("-a", "--augmented", action="store_true",
                         help="Use the augmented feature set for better performance")
+    parser.add_argument("-m", "--mhs", action="store_true",
+                        help="Use more OFF training data")
 
     return parser.parse_args()
+
+
+def create_new_data():
+    """Create additional offensive data"""
+    # Retrieve dataset
+    dataset = datasets.load_dataset('ucberkeley-dlab/measuring-hate-speech')
+    df = dataset['train'].to_pandas()
+
+    # Retrieve two relevant columns
+    augment_data = df[['hate_speech_score', 'text']]
+    augment_data = augment_data.values.tolist()
+
+    # Create 4000 lines of new data and save
+    documents, labels = [], []
+    counter = 0
+
+    for line in augment_data:
+        if counter < 4000 and line[0] >= 0.5:
+            counter += 1
+            cleaned = re.sub('@\w+', '@USER', line[1])
+            cleaned = re.sub('https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)', 'URL', cleaned)
+            documents.append(cleaned.translate(str.maketrans('', '', string.punctuation)))
+            labels.append('OFF')
+
+    return documents, labels
 
 
 def read_corpus(corpus_file):
@@ -51,11 +81,9 @@ def read_corpus(corpus_file):
     with open(corpus_file, encoding='utf-8') as f:
         for line in f:
             tokens = line.strip().split('\t')
-            documents.append(tokens[0])
+            documents.append(tokens[0].translate(str.maketrans('', '', string.punctuation)))
             labels.append(tokens[1])
 
-    print('*** Label Distribution ' + corpus_file + ' ***')
-    print(Counter(labels))
     return documents, labels
 
 
@@ -103,6 +131,14 @@ if __name__ == "__main__":
     # Load data used for experiments
     X_train, Y_train = read_corpus(args.train_file)
     X_test, Y_test = read_corpus(args.dev_file)
+
+    # Create additional OFF data if argument is given
+    if args.mhs:
+        X_train_aug, Y_train_aug = create_new_data()
+        X_train = X_train + X_train_aug
+        Y_train = Y_train + Y_train_aug
+
+    print('Train label distribution: ', Counter(Y_train))
 
     # Choose vectorizer and features with hyperparameters for tuning
     if args.augmented:
